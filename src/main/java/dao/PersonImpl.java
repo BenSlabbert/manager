59f4bf3ecf14542992c33df869580c23d160ab5a
Benjamin Slabbert <benjamin@impactradius.com>
2016-10-29 10:40:54 +0200

    implemented dao methods in PersonImpl -> output added in Manager

--- src/main/java/dao/PersonImpl.java
+++ src/main/java/dao/PersonImpl.java
@@ -1,18 +1,20 @@
 package dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import java.util.List;
+import javax.persistence.Query;
 
 
 /**
  * Created by benjamin on 2016/10/23.
  */
 public class PersonImpl implements PersonDAO {
-
-    private EntityManagerFactory emf =
-            Persistence.createEntityManagerFactory("myDb");
+    
+    // TODO : replace EntityManagerFactory with a spring bean
+    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("myDb");
     
     @Override
     public Person getPersonById(Integer id) {
@@ -25,12 +27,27 @@ public class PersonImpl implements PersonDAO {
     }
     
     @Override
-    public List<Person> searchByFirstName(String name) {
-        return null;
+    public List<Person> searchByFirstName(String searchFirstName) {
+
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        Query q = em.createQuery("select p from Person p where p.name = :name");
+        q.setParameter("name", searchFirstName);
+        List<Person> person = q.getResultList();
+        em.getTransaction().commit();
+        em.close();
+        return person;
     }
-    
+
     @Override
-    public List<Person> searchByLastName(String name) {
-        return null;
+    public List<Person> searchByLastName(String searchLastName) {
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        Query q = em.createQuery("select p from Person p where p.surname = :name");
+        q.setParameter("name", searchLastName);
+        List<Person> person = q.getResultList();
+        em.getTransaction().commit();
+        em.close();
+        return person;
     }
 }
